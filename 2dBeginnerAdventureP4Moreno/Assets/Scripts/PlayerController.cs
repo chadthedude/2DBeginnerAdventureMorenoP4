599c0d496aca23d66de5c670e9644e646034a4d2
chadthedude <724626@student.dusd.net>
2024-10-30 11:44:04 -0700

    blocked movement
    I added box colliders to prefabs on my game objects to block movement on my player character ruby

--- 2dBeginnerAdventureP4Moreno/Assets/Scripts/PlayerController.cs
+++ 2dBeginnerAdventureP4Moreno/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2D;
+    float horizontal;
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.0f * vertical * Time.deltaTime;
-        transform.position = position;
+       
 
     
     }
-}
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
+        
+        rigidbody2D.MovePosition(position);
+    }
+
+} 
