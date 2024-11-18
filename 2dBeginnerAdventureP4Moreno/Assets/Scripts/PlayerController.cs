733bcac7b3e4cdd6bd21ad6311f4ae60e154e18b
chadthedude <724626@student.dusd.net>
2024-11-18 11:59:13 -0800

    finished NPC and started audio
    Finished DuckLady's dialog and started to add audio to characters and game objects. You left on 15:22 for the video "Audio".

--- 2dBeginnerAdventureP4Moreno/Assets/Scripts/PlayerController.cs
+++ 2dBeginnerAdventureP4Moreno/Assets/Scripts/PlayerController.cs
@@ -25,6 +25,10 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
+    AudioSource audioSource;
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +36,7 @@ public class PlayerController : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
        
     }
 
@@ -65,6 +70,19 @@ public class PlayerController : MonoBehaviour
         {
             Launch();
         }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if(hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if(character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
       
 
     
@@ -82,6 +100,7 @@ public class PlayerController : MonoBehaviour
         if(amount < 0)
         {
             animator.SetTrigger("Hit");
+            
 
             if (isInvincible)
             {
@@ -89,6 +108,7 @@ public class PlayerController : MonoBehaviour
             }
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            PlaySound(hitSound);
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         UIHealthBar.instance.SetValue(currentHealth/(float)maxHealth);
@@ -101,6 +121,11 @@ public class PlayerController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        PlaySound(throwSound);
     }
 
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip); 
+    }
 } 
